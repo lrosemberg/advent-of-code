b03be6248d1e799346d7d95377a8932d2d79fcd8
Leandro Rosemberg <leandro.rosemberg@buser.com.br>
2022-12-01 12:26:34 -0300

    refatoracaozinha

--- 2022/01/solution.js
+++ 2022/01/solution.js
@@ -5,24 +5,20 @@ const { join } = require('path')
 const file = fs.readFileSync(join(__dirname, 'input.txt'), 'utf8')
 
 // Setup
-const backpacks = file.split('\n\n').map(items => {
-  const backpack = {
+const backpacks = file.split('\n\n').map(items => (
+  {
     foods: items.split('\n'),
     total: 0
-  }
-  return backpack
-})
-
-function sum_kcals (backpack) {
-  backpack.total = backpack.foods.reduce((a, b) => a + +b, 0)
-}
+  })
+)
 
 for (let backpack of backpacks) {
-  sum_kcals(backpack)
+  backpack.total = backpack.foods.reduce((a, b) => a + +b, 0)
 }
 
 // Part 1
 const bigger = backpacks.reduce((prev, curr) => Math.max(prev, curr.total), 0)
+
 console.log(`Part 01: ${ bigger }`)
 
 // Part 2
@@ -30,4 +26,5 @@ const n = 3
 const sorted = backpacks.sort((a, b) => b.total - a.total)
 const topn = sorted.slice(0, n)
 const sum = topn.reduce((prev, curr) => prev += curr.total, 0)
+
 console.log(`Part 02: ${ sum }`)
